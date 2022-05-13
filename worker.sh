4b39d40de836880e0c4b82450b6624599dd191fe
Karpenko Andrei <akarpenko@colvir.com>
2022-05-13 17:26:07 +0300

    create project

---
+++ worker.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -eu
+ 
+#kubectl binary setup
+apk add -U curl
+curl -L -o /usr/bin/kubectl "https://storage.googleapis.com/kubernetes-release/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl"
+chmod +x /usr/bin/kubectl
+kubectl version --client
+ 
+#kubectl connection setup
+kubectl config set-cluster default-cluster --insecure-skip-tls-verify=true --server="${KUBE_URL}"
+if [ -z ${KUBE_USER+x} ]; then
+    echo "TOKEN Auth used"
+    kubectl config set-credentials default-admin --token="${KUBE_TOKEN}"
+else
+    echo "LOGIN/PASSWORD Auth used"
+    kubectl config set-credentials default-admin --username="${KUBE_USER}" --password="${KUBE_PASSWORD}"
+fi
+ 
+kubectl config set-context default-system --user=default-admin --namespace="${KUBE_NAMESPACE}" --cluster=default-cluster
+kubectl config use-context default-system
+kubectl cluster-info
+ 
+#manifests setup
+cd manifests/
+sed -i "s%KUBE_NAMESPACE%${KUBE_NAMESPACE}%" CronJob.yml
+sed -i "s%DEPLOYMENT_NAME%${DEPLOYMENT_NAME}%" CronJob.yml
+sed -i "s%KUBE_NAMESPACE%${KUBE_NAMESPACE}%" RoleBinding.yml
+sed -i "s%DEPLOYMENT_NAME%${DEPLOYMENT_NAME}%" Role.yml
+sed -i "s%KUBE_NAMESPACE%${KUBE_NAMESPACE}%" Role.yml
+sed -i "s%KUBE_NAMESPACE%${KUBE_NAMESPACE}%" ServiceAccount.yml
+ 
+#manifests deploy
+kubectl apply -R -f .
